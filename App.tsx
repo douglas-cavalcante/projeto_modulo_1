deba48478fd37d5b757679ac620abe145268c6d2
Henrique Douglas <henriquedouglas2803@gmail.com>
2024-10-23 22:07:57 -0300

    adicioana exemplo de cadastro de movimentacao

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import { Home } from './src/Home';
 import { StatusBar } from 'react-native';
 import { ListaUsuarios } from './src/ListaUsuarios';
 import NovoUsuario from './src/NovoUsuario';
+import CadastroMovimentacao from './src/CadastroMovimentacao';
 
 const Stack = createStackNavigator()
 
@@ -13,11 +14,12 @@ export default function App() {
   return (
     <NavigationContainer>
       <StatusBar />
-       <Stack.Navigator initialRouteName='Login'>
+       <Stack.Navigator initialRouteName='CadastroMovimentacao'>
            <Stack.Screen name="Login" component={Login} options={{header: () => <></>}} />
            <Stack.Screen name="Home" component={Home} />
            <Stack.Screen name="ListaUsuarios" component={ListaUsuarios} />
            <Stack.Screen name="NovoUsuario" component={NovoUsuario} />
+           <Stack.Screen name="CadastroMovimentacao" component={CadastroMovimentacao} />
        </Stack.Navigator>
     </NavigationContainer>
   );
